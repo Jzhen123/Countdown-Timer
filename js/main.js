6d374954ab689439e7bad1c3c31a0d72c8467a59
Jzhen123 <jiayuzheng01@gmail.com>
2021-02-19 16:58:15 -0500

    timer works except for seconds

--- js/main.js
+++ js/main.js
@@ -5,13 +5,27 @@ setInterval(calculateTimeLeft, 100);
 function calculateTimeLeft() {
 
     var currentDate = new Date();
-    var endDate = Date.prototype.getTime("April 9, 2021 00:00:00")
+    var endDate = new Date("Apr 9, 2021 00:00:00").getTime();
 
     timeLeft = endDate - currentDate;
 
-    var seconds = timeLeft * 1000;
-    
+    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000* 60 * 60));
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % 60) / 1000);
 
+    document.getElementById("days").innerHTML = days + " Days";
+    document.getElementById("hours").innerHTML = hours + " Hours";
+    document.getElementById("minutes").innerHTML = minutes + " Minutes";
+    document.getElementById("seconds").innerHTML = seconds + " Seconds";
+
+    if (timeLeft < 0) {
+        document.getElementById("days").innerHTML = "";
+        document.getElementById("hours").innerHTML = "";
+        document.getElementById("minutes").innerHTML = "";
+        document.getElementById("seconds").innerHTML = "";
+        document.getElementById("happyBday").innerHTML = "HAPPY BIRTHDAY!!!";
+    }
 
 
 
