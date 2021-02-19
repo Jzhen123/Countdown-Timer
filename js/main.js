a8cfc31ac7900b70a93ab5fe45b445dfef76cf40
Jzhen123 <jiayuzheng01@gmail.com>
2021-02-19 18:04:08 -0500

    Almost fully functional countdown timer

--- js/main.js
+++ js/main.js
@@ -1,35 +1,33 @@
+var currentDate = new Date();
+var endDate = new Date("Apr 9, 2021 00:00:00").getTime();
+var countdownDays = document.querySelector('.countdownDays');
+var countdownHours = document.querySelector('.countdownHours');
+var countdownMinutes = document.querySelector('.countdownMinutes');
+var countdownSeconds = document.querySelector('.countdownSeconds');
 
 
-setInterval(calculateTimeLeft, 100);
+setInterval(calculateTimeLeft(), 100);
 
 function calculateTimeLeft() {
-
-    var currentDate = new Date();
-    var endDate = new Date("Apr 9, 2021 00:00:00").getTime();
 
     timeLeft = endDate - currentDate;
 
     var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000* 60 * 60));
+    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeLeft % 60) / 1000);
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    document.getElementById("days").innerHTML = days + " Days";
-    document.getElementById("hours").innerHTML = hours + " Hours";
-    document.getElementById("minutes").innerHTML = minutes + " Minutes";
-    document.getElementById("seconds").innerHTML = seconds + " Seconds";
+    countdownDays.innerHTML = days;
+    countdownHours.innerHTML = hours + " Hours";
+    countdownMinutes.innerHTML = minutes + " Minutes";
+    countdownSeconds.innerHTML = seconds + " Seconds";
 
     if (timeLeft < 0) {
-        document.getElementById("days").innerHTML = "";
-        document.getElementById("hours").innerHTML = "";
-        document.getElementById("minutes").innerHTML = "";
-        document.getElementById("seconds").innerHTML = "";
+        document.getElementById("countdownDays").innerHTML = "";
+        document.getElementById("countdownHours").innerHTML = "";
+        document.getElementById("countdownMinutes").innerHTML = "";
+        document.getElementById("countdownSecond").innerHTML = "";
         document.getElementById("happyBday").innerHTML = "HAPPY BIRTHDAY!!!";
     }
-
-
-
-
-
 }
-
+calculateTimeLeft();
