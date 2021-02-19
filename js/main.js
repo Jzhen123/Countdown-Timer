0fa2ea66dc54a89416631ea179cc126c5310d20d
Jzhen123 <jiayuzheng01@gmail.com>
2021-02-19 16:38:31 -0500

    Added the meat of the countdown timer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-var currentDate = new Date();
-var endDate = Date.prototype.getTime("April 9, 2021 00:00:00")
 
-timeLeft = endDate - currentDate;
+
+setInterval(calculateTimeLeft, 100);
+
+function calculateTimeLeft() {
+
+    var currentDate = new Date();
+    var endDate = Date.prototype.getTime("April 9, 2021 00:00:00")
+
+    timeLeft = endDate - currentDate;
+
+    var seconds = timeLeft * 1000;
+    
+
+
+
+
+
+
+}
+
