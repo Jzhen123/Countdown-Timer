af879cbd27c1d8ebc71a4414e8924a117fdc7be0
Jzhen123 <jiayuzheng01@gmail.com>
2021-03-16 18:44:10 -0400

    Fixed auto update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-var currentDate = new Date();
 var endDate = new Date("Apr 9, 2021 00:00:00").getTime();
 var countdownDays = document.querySelector('.countdownDays');
 var countdownHours = document.querySelector('.countdownHours');
@@ -6,22 +5,22 @@ var countdownMinutes = document.querySelector('.countdownMinutes');
 var countdownSeconds = document.querySelector('.countdownSeconds');
 
 
-setInterval(calculateTimeLeft, 1000);
 
 function calculateTimeLeft() {
-
+    
+    var currentDate = new Date();
     timeLeft = endDate - currentDate;
-
+    
     var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
+    
     countdownDays.innerHTML = days;
     countdownHours.innerHTML = hours + " Hours";
     countdownMinutes.innerHTML = minutes + " Minutes";
     countdownSeconds.innerHTML = seconds + " Seconds";
-
+    
     if (timeLeft < 0) {
         document.getElementById("countdownDays").innerHTML = "";
         document.getElementById("countdownHours").innerHTML = "";
@@ -30,4 +29,5 @@ function calculateTimeLeft() {
         document.getElementById("happyBday").innerHTML = "HAPPY BIRTHDAY!!!";
     }
 }
-calculateTimeLeft();
+
+setInterval(calculateTimeLeft, 1000);
